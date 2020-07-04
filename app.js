b3beb688ec5b48c53248417f379482862407d9fa
Cooper Pearson <mptprogesterone@gmail.com>
2020-07-04 14:38:59 -0500

    add book clear book function

--- app.js
+++ app.js
@@ -6,12 +6,30 @@ function Book(title, author, isbn) {
 }
 
 //UI Constructor
-function UI() {
+function UI() { }
 
-}
+
 //Add Book to list
 UI.prototype.addBookToList = function (book) {
+    const list = document.getElementById('book-list');
+    //Create tr element
+    const row = document.createElement('tr');
+    //Insert cols
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+    `;
 
+    list.appendChild(row);
+}
+
+//Clear Fields
+UI.prototype.clearFields() = function () {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
 }
 
 //Event listeners
@@ -25,10 +43,13 @@ document.getElementById('book-form').addEventListener('submit',
         const book = new Book(title, author, isbn);
 
         //Instantiate UI
-        const UI = new UI();
+        const ui = new UI();
 
         //Add book to list
         ui.addBookToList(book);
+
+        //Clear Fields
+        ui.clearFields();
 
         e.preventDefault();
     });
